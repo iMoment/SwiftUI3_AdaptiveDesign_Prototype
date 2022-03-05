28ced70ccd484a4d2faa1dcc0ff0363f64123768
Stanley Pan <imoment.developer@gmail.com>
2022-03-04 21:17:37 -0500

    Initial Commit

---
+++ ResponsiveUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ResponsiveUI
+//
+//  Created by Stanley Pan on 2022/03/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ResponsiveUI/ResponsiveUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResponsiveUIApp.swift
+//  ResponsiveUI
+//
+//  Created by Stanley Pan on 2022/03/04.
+//
+
+import SwiftUI
+
+@main
+struct ResponsiveUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
